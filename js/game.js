bbbad55d2bd3d42702bd62835e25c4e1e270ea85
ocgrey <ocgrey@gmail.com>
2015-06-15 14:06:21 -0400

    t

--- js/game.js
+++ js/game.js
@@ -8,8 +8,8 @@ var canvas = $('#gameCanvas')[0].getContext('2d');
 var image = new Image();
 image.src ="http://3.bp.blogspot.com/-cg1jtrxaZ8Y/Ufl5SmFUVaI/AAAAAAAAAzY/KBxNVcMmOB0/s1600/F5S4.png";
 
-var playerX = (canvasWidth/2) - (image.width/2);
-var playerY = (canvasHeight/2)  - (image.height/2);
+var playerX = (canvasWidth/ 2) - (image.width/ 2);
+var playerY = (canvasHeight/ 2)  - (image.height/ 2);
 
 var FPS = 30;
 setInterval(function() {
